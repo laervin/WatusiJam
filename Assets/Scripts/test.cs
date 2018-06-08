56dc4bb77e90e5245618735074dc1a72099cb81a
Garrett <Garrettdeblois13@yahoo.com>
2018-06-07 21:06:43 -0500

    First test

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+
+	public GameObject rock;
+	public Rigidbody2D rigidbody;
+	public int speed;
+	public int startingValue = 0;
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody2D> ();
+	}
+
+	public void click()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		rigidbody.AddForce(Vector2.right * speed);
+		rigidbody.AddForce (Vector2.up * 200);
+
+		if (rock.transform.position.x >= 50) {
+
+			rigidbody.gravityScale = 10;
+		}
+		else if (rock.transform.position.x >= -25) {
+
+			rigidbody.gravityScale = 8;
+		}
+			else
+		if (rock.transform.position.x >= 0) {
+
+			rigidbody.gravityScale = 4;
+		}
+			else	
+		if (rock.transform.position.x >= -25) {
+
+			rigidbody.gravityScale = 2;
+		}
+
+	}
+}
